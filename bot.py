31679de3752e1848359706855958869b382e56a2
Hillebrand van de Groep <github@hillebrand.io>
2018-05-05 23:25:27 +0200

    added testing /trein function

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import json
+import datetime
+import dvs
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -14,10 +16,65 @@ logger = logging.getLogger(__name__)
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
-    update.message.reply_text('Hello ovdingenbot!')
+    update.message.reply_text('Hello ovdingenbot! /help')
+
+def help(bot, update):
+    update.message.reply_text('`/trein <trein_number>` or `/trein <trein_number> <dd-mm-yy>`')
 
 def trein(bot, update, args):
-    update.message.reply_text(str(args))
+    if len(args) > 2:
+        update.message.reply_text("Too many arguments. Try using /help.")
+        return False
+    if len(args) is 0:
+        update.message.reply_text("Too few arguments. Try using /help.")
+        return False
+    
+    treinnummer = args[0]
+    if len(args) is 1:
+        date = datetime.datetime.today().strftime('%Y-%m-%d')
+    else:
+        date = args[1]
+
+    treininfo = dvs.train("https://dvs.ovdingen.nl", date, treinnummer)
+    
+    if treininfo['via']:
+        header = "*{} {} {} richting {} via {}*\n".format(treininfo['vervoerder'], treininfo['soort'], treininfo['treinNr'], treininfo['bestemming'], treininfo['via'])
+    else:
+        header = "*{} {} {} richting {}*\n".format(treininfo['vervoerder'], treininfo['soort'], treininfo['treinNr'], treininfo['bestemming'])
+
+
+    if len(treininfo['vleugels']) > 1:
+        vleugeltekst = "trein bevat > 1 vleugel, not yet implemented"
+    else:
+        stops = "*Stops:*\n"
+        vleugel = treininfo['vleugels'][0]
+        for stop in vleugel['stopstations']:
+            if stop['aankomst'] == None and stop['vertrek']: # Beginstation
+                stops += "_{}_ ({}) V {} spoor {}\n".format(stop['naam'], stop['code'].upper(), stop['vertrek'], stop['vertrekspoor'])
+            elif stop['vertrek'] == None and stop['aankomst']: # Eindstation
+                stops += "_{}_ ({}) A {} spoor {}\n".format(stop['naam'], stop['code'].upper(), stop['aankomst'], stop['aankomstspoor'])
+            else: # Tussenstation
+                stops += "_{}_ ({}) A {} V {} spoor {}\n".format(stop['naam'], stop['code'].upper(), stop['aankomst'], stop['vertrek'], stop['vertrekspoor'])
+        
+        mat_text = "*Materieel:*\n"
+        if len(vleugel['mat']) is 0:
+            mat_text += "Materieelinfo niet beschikbaar"
+        else:
+            for materieel in vleugel['mat']:
+                if materieel[2] is None: # geen mat-nr
+                    mat_text += "{}, eindbestemming {}".format(materieel[0], materieel[1])
+                else:
+                    mat_text += "{} {}, eindbestemming {}".format(materieel[0], materieel[2], materieel[1])
+        
+
+
+        vleugeltekst = stops + "\n" + mat_text
+            
+
+    
+    message = header + vleugeltekst
+
+    update.message.reply_text(message, parse_mode="Markdown")
 
 def main():
     """Start the bot."""
@@ -32,6 +89,7 @@ def main():
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("trein", trein, pass_args=True))
 
     # Start the Bot
